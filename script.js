c66b8a789d195945c7f00ef094c942d1699c13d1
staxxtec <144523338+staxxtec@users.noreply.github.com>
2024-02-07 15:52:45 +0900

    Update script.js
    go store

--- script.js
+++ script.js
@@ -16,3 +16,6 @@ const goldText = document.querySelector("#goldText");
 const monsterStats = document.querySelector("#monsterStats");
 const monsterName = document.querySelector("#monsterName");
 const monsterHealthText = document.querySelector("#monsterHealth");  
+function goStore() {
+  
+}
