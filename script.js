a8dfa1d06fe68c7ad11660385579dd29f71445f7
staxxtec <144523338+staxxtec@users.noreply.github.com>
2024-02-08 08:47:35 +0900

    Update script.js
    declare button1

--- script.js
+++ script.js
@@ -17,6 +17,8 @@ const monsterStats = document.querySelector("#monsterStats");
 const monsterName = document.querySelector("#monsterName");
 const monsterHealthText = document.querySelector("#monsterHealth");  
 //initialize buttons
+button1.onclick = goStore
+
 function goStore() {
     console.log("Going to store.");
 }
