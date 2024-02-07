3e57da098df9dffb16e9a6950ea3c0da24ba323a
staxxtec <144523338+staxxtec@users.noreply.github.com>
2024-02-07 20:19:55 +0900

    Update script.js
    going to cave

--- script.js
+++ script.js
@@ -19,3 +19,6 @@ const monsterHealthText = document.querySelector("#monsterHealth");
 function goStore() {
     console.log("Going to store.");
 }
+function goCave() {
+    console.log("Going to cave.");
+  }
