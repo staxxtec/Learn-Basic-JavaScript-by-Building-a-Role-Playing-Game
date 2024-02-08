910b03ee1c1574ab6282f8524b15c70ca9f11198
staxxtec <144523338+staxxtec@users.noreply.github.com>
2024-02-08 22:45:51 +0900

    Update script.js

--- script.js
+++ script.js
@@ -20,16 +20,17 @@ const monsterHealthText = document.querySelector("#monsterHealth");
 button1.onclick = goStore;
 button2.onclick = goCave;
 button3.onclick = fightDragon;
+function update(location) {
+}
 
 function goTown () {
     button1.innerText = "Go to store";
-  button2.innerText = "Go to cave";
-  button3.innerText = "Fight dragon";
-  button1.onclick = goStore;
-  button2.onclick = goCave;
-  button3.onclick = fightDragon;
-  text.innerText = "You are in the town square. You see a sign that says Store.";
-  text.innerText = "You enter the store.";
+    button2.innerText = "Go to cave";
+    button3.innerText = "Fight dragon";
+    button1.onclick = goStore;
+    button2.onclick = goCave;
+    button3.onclick = fightDragon;
+    text.innerText = "You are in the town square. You see a sign that says \"Store\".";
 }
 function goStore() {
     button1.innerText = "Buy 10 health (10 gold)";
