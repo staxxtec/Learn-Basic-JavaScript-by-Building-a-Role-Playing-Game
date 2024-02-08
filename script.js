b2743994aa769a59fa13c759daead2fccae4df00
staxxtec <144523338+staxxtec@users.noreply.github.com>
2024-02-08 22:23:06 +0900

    Update script.js

--- script.js
+++ script.js
@@ -22,12 +22,13 @@ button2.onclick = goCave;
 button3.onclick = fightDragon;
 
 function goTown () {
-    button1.innerText = "Buy 10 health (10 gold)";
-  button2.innerText = "Buy weapon (30 gold)";
-  button3.innerText = "Go to town square";
-  button1.onclick = buyHealth;
-  button2.onclick = buyWeapon;
-  button3.onclick = goTown;
+    button1.innerText = "Go to store";
+  button2.innerText = "Go to cave";
+  button3.innerText = "Fight dragon";
+  button1.onclick = goStore;
+  button2.onclick = goCave;
+  button3.onclick = fightDragon;
+  text.innerText = "You are in the town square. You see a sign that says Store.";
   text.innerText = "You enter the store.";
 }
 function goStore() {
