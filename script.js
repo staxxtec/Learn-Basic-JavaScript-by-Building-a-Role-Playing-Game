daf80f43f4fa991e7d3278655edf758d6d85bd9a
staxxtec <144523338+staxxtec@users.noreply.github.com>
2024-02-12 22:02:58 +0900

    Update script.js
    Add a third object to the locations array. Give it the same properties as the other two objects.
    
    Set name to cave. Set button text to an array with the strings "Fight slime", "Fight fanged beast", and "Go to town square". Set the "button functions" to an array with the variables fightSlime, fightBeast, and goTown. Set the text property to "You enter the cave. You see some monsters.".

--- script.js
+++ script.js
@@ -25,7 +25,12 @@ const locations = [
       { name: "store",
       "button text": ["Buy 10 health (10 gold)", "Buy weapon (30 gold)", "Go to town square"],
       "button functions": [buyHealth, buyWeapon, goTown],
-      text: "You enter the store."}
+      text: "You enter the store."},
+      {  name: "cave",
+    "button text": ["Fight slime", "Fight fanged beast", "Go to town square"],
+    "button functions": [fightSlime, fightBeast, goTown],
+    text: "You enter the cave. You see some monsters."
+  }
   
     ];
 
