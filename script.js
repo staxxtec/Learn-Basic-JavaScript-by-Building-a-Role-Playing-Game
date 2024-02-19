11e4dfded85a668c68fb5d4308eede7b328bf555
staxxtec <144523338+staxxtec@users.noreply.github.com>
2024-02-19 22:35:15 +0900

    Update script.js
    Step 105
    
    Now use the += operator to add the string " In your inventory you have: " and the contents of inventory to the text.innerText. Make sure to include the space at the beginning and end of the " In your inventory you have: " string.

--- script.js
+++ script.js
@@ -115,6 +115,7 @@ function goCave() {
       goldText.innerText = gold;
       let currentWeapon = inventory.shift();
       text.innerText = "You sold a " + currentWeapon + ".";
+      text.innerText += " In your inventory you have: " + inventory;
     }
   }
   
