19cec380a5c846de7fa1169c09f0b3a46302859e
staxxtec <144523338+staxxtec@users.noreply.github.com>
2024-02-19 21:46:31 +0900

    Update script.js
    Step 98
    
    Once a player has the most powerful weapon, you can give them the ability to sell their old weapons.
    
    In the outer else statement, set button2.innerText to "Sell weapon for 15 gold". Also set button2.onclick to the function name sellWeapon.

--- script.js
+++ script.js
@@ -105,6 +105,8 @@ function goCave() {
           } 
         } else {
       text.innerText = "You already have the most powerful weapon!";
+      button2.innerText = "Sell weapon for 15 gold";
+  button2.onclick = sellWeapon; 
     }
   }
   
