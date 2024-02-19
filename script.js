81a50464f9b60f483bc50e7384d709ab18619307
staxxtec <144523338+staxxtec@users.noreply.github.com>
2024-02-19 21:36:24 +0900

    Update script.js
    Step 97
    
    Add an else statement for your outer if statement. Inside this new else statement, set text.innerText to "You already have the most powerful weapon!".

--- script.js
+++ script.js
@@ -100,13 +100,13 @@ function goCave() {
       text.innerText = "You now have a " +  newWeapon + ".";
       inventory.push(newWeapon);
       text.innerText += " In your inventory you have: " + inventory;
-    }
-        else {
+    } else {
             text.innerText = "You do not have enough gold to buy a weapon.";
           } 
-      
+        } else {
+      text.innerText = "You already have the most powerful weapon!";
     }
-  
+  }
   
   function  fightSlime() {
 
