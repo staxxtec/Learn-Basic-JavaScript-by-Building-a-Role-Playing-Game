31e447a9c1b7945fa81e50cc9f2123c2d9e2b0cb
staxxtec <144523338+staxxtec@users.noreply.github.com>
2024-02-23 19:17:50 +0900

    Update script.js
    Step 115
    
    By default, the HTML element that shows the monster's stats has been hidden with CSS. When the player clicks the "Fight dragon" button, the monster's stats should be displayed. You can accomplish this by using the style and display properties on the monsterStats element.
    
    The style property is used to access the inline style of an element and the display property is used to set the visibility of an element.
    
    Here is an example of how to update the display for a paragraph element:
    
    const paragraph = document.querySelector('p');
    paragraph.style.display = 'block';
    
    Display the monsterStats element by updating the display property of the style property to block.

--- script.js
+++ script.js
@@ -149,6 +149,8 @@ function goCave() {
   function goFight() {
     update(locations [3]);
     monsterHealth = monsters[fighting].health;
+    monsterStats = document.querySelector('p');
+monsterStats.style.display = 'block';
 
   }
   function  attack() {
