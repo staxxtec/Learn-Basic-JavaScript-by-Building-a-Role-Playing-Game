a81bc44eccc421fcd3a7dd402cd56ce5b6e113cf
staxxtec <144523338+staxxtec@users.noreply.github.com>
2024-02-23 20:10:57 +0900

    Update script.js
    Step 120
    
    Set monsterHealth to monsterHealth minus the power of the player's current weapon.
    
    Remember that you can access the power of the player's current weapon using weapons[currentWeapon].power.

--- script.js
+++ script.js
@@ -158,6 +158,7 @@ function goCave() {
     text.innerText = "The "+ monsters[fighting].name + " attacks."
     text.innerText += " You attack it with your " + weapons[currentWeapon].name + ".";
     health -= monsters[fighting].level;
+    monsterHealth -= weapons[currentWeapon].power;
 
   }
   function dodge() {
