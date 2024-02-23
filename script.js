d331142b2eaf644659ffe68ebcb261a49294d3a5
staxxtec <144523338+staxxtec@users.noreply.github.com>
2024-02-23 19:32:51 +0900

    Update script.js
    Step 116
    
    Now, you will need to update the text for the current monster's name and health.
    
    Start by assigning monsters[fighting].name to the innerText property of monsterName. Then, assign monsterHealth to the innerText property of monsterHealthText.

--- script.js
+++ script.js
@@ -150,6 +150,8 @@ function goCave() {
     update(locations [3]);
     monsterHealth = monsters[fighting].health;
     monsterStats.style.display = 'block';
+    monsterName.innerText = monsters[fighting].name;
+  monsterHealthText.innerText = monsterHealth;
 
   }
   function  attack() {
