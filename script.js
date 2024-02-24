3cebba7c81406aa34f3f80f32cdc5c6f1ba42b02
staxxtec <144523338+staxxtec@users.noreply.github.com>
2024-02-24 16:45:42 +0900

    Update script.js
    Step 122
    
    Update healthText.innerText and monsterHealthText.innerText to equal health and monsterHealth.

--- script.js
+++ script.js
@@ -159,7 +159,8 @@ function goCave() {
     text.innerText += " You attack it with your " + weapons[currentWeapon].name + ".";
     health -= monsters[fighting].level;
     monsterHealth -= weapons[currentWeapon].power + Math.floor(Math.random () * xp) + 1;
-
+    healthText.innerText = health;
+  monsterHealthText.innerText = monsterHealth;
   }
   function dodge() {
     
