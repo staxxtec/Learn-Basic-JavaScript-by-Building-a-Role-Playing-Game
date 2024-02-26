494cbde6bd12fa258285e419d28ed92c3d16ec41
staxxtec <144523338+staxxtec@users.noreply.github.com>
2024-02-26 21:03:29 +0900

    Update script.js
    Step 144
    
    Log the value of hit to the console to use in debugging. Remember that you can do this with console.log().

--- script.js
+++ script.js
@@ -192,6 +192,7 @@ function goCave() {
   }
   function getMonsterAttackValue (level) {
     const hit = (level * 5) - (Math.floor(Math.random() * xp));
+    console.log(hit);
   }
   
   function dodge() {
