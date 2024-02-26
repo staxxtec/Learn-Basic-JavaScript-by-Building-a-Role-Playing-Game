0abaea3fc6591036031beafce3e9fe4e857a4d1b
staxxtec <144523338+staxxtec@users.noreply.github.com>
2024-02-26 21:37:14 +0900

    Update script.js
    Step 146
    
    If you play the game in its current state you might notice a bug. If your xp is high enough, the getMonsterAttackValue function will return a negative number, which will actually add to your total health when fighting a monster! You can fix this issue by using a ternary operator to ensure negative values are not returned.
    
    The ternary operator is a conditional operator and can be used as a one-line if-else statement. The syntax is: condition ? expressionIfTrue : expressionIfFalse.
    
    Here is an example of returning a value using an if-else statement and a refactored example using a ternary operator:
    
    // if-else statement
    if (score > 0) {
      return score
    } else {
      return default_score
    }
    
    // ternary operator
    score > 0 ? score : default_score
    
    In getMonsterAttackValue, change return hit to a ternary operator that returns hit if hit is greater than 0, or returns 0 if it is not.

--- script.js
+++ script.js
@@ -193,7 +193,7 @@ function goCave() {
   function getMonsterAttackValue (level) {
     const hit = (level * 5) - (Math.floor(Math.random() * xp));
     console.log(hit);
-    return hit
+    return hit > 0 ? hit : 0;
   }
   
   function dodge() {
