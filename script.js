0ac21efbef5ddc4fd38ce7f875cf1be014d19ff3
staxxtec <144523338+staxxtec@users.noreply.github.com>
2024-02-29 23:23:55 +0900

    Update script.js
    Step 150
    
    Now create the isMonsterHit function. This will return a boolean value (true or false) to be used in your if statement. Return the result of the comparison Math.random() > .2.

--- script.js
+++ script.js
@@ -202,7 +202,9 @@ function goCave() {
     console.log(hit);
     return hit > 0 ? hit : 0;
   }
-  
+  function isMonsterHit() {
+    return Math.random() > .2;
+  }
   function dodge() {
     text.innerText = "You dodge the attack from the " + monsters[fighting].name;
   }
