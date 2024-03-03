a4cfac6f5b8553e6fdbb9c46a106dfa7a074feaa
staxxtec <144523338+staxxtec@users.noreply.github.com>
2024-03-03 14:45:04 +0900

    Update script.js

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ const locations = [
     {name: "town square",
       "button text": ["Go to store", "Go to cave", "Fight dragon"],
       "button functions": [goStore, goCave, fightDragon],
-      text: "You are in the town square. You see a sign that says \"Store\".",
+      text: "You are in the town square. You see a sign that says \"Store\"."
     },
       { name: "store",
       "button text": ["Buy 10 health (10 gold)", "Buy weapon (30 gold)", "Go to town square"],
