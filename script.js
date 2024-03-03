1222f00a464d95e61fc6881e3b0817a328a86d28
staxxtec <144523338+staxxtec@users.noreply.github.com>
2024-03-03 13:38:38 +0900

    Update script.js
    Step 172
    
    Now add an else statement. Inside, add "Wrong! You lose 10 health!" to the end of text.innerText. Subtract 10 from health and update healthText.innerText.

--- script.js
+++ script.js
@@ -255,17 +255,22 @@ function goCave() {
     pick(8);
   }
   function pick(guess) {
-    for (let i = 0; i < 10; i++) {
-      if(numbers.includes(guess));{
-        text.innerText += "Right! You win 20 gold!";
-goldText.innerText = gold += 20;
-      }
-    }
-    
     const numbers = [];
-    while (numbers.length < 10){
-    numbers.push(Math.floor(Math.random() * 11));
+    while (numbers.length < 10) {
+      numbers.push(Math.floor(Math.random() * 11));
     }
-    text.innerText = "You picked " + guess +  ". Here are the random numbers:\n";
-    text.innerText += numbers[i] + "\n"
+    text.innerText = "You picked " + guess + ". Here are the random numbers:\n";
+    for (let i = 0; i < 10; i++) {
+      text.innerText += numbers[i] + "\n";
+    }
+    if (numbers.includes(guess)) {
+      text.innerText += "Right! You win 20 gold!";
+      gold += 20;
+      goldText.innerText = gold;
+    } else {
+      text.innerText += "Wrong! You lose 10 health!";
+      health -= 10;
+      healthText.innerText = health;
+  
+    }
   }
